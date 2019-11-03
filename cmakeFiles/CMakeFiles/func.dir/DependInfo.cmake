886b9c6ab8b501dadfc2b7b45dfac8846f5daedd
Artem Sevruk <artem55660120072013@gmail.com>
2019-11-03 18:44:32 +0200

    Finished Labwork4

---
+++ cmakeFiles/CMakeFiles/func.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/amooyts/lab4/factorial.cpp" "/home/amooyts/cmakeFiles/CMakeFiles/func.dir/factorial.cpp.o"
+  "/home/amooyts/lab4/hello.cpp" "/home/amooyts/cmakeFiles/CMakeFiles/func.dir/hello.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmakeFiles/CMakeFiles/func.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/func.dir/hello.cpp.o"
+  "CMakeFiles/func.dir/factorial.cpp.o"
+  "libfunc.pdb"
+  "libfunc.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/func.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
